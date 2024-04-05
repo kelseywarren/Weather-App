f7d48fedcfb3221fe18816aba4e74939e7739cf3
kelseywarren <warrenkelsey@outlook.com>
2024-04-05 07:43:51 -0500

    added math.floor to round temperature and wind values

--- main.js
+++ main.js
@@ -32,10 +32,10 @@ function getWeatherData(city) {
       .then((response) => response.json())
       .then((data) => {
           cityName.innerHTML = `${data.name}`;
-          temperature.innerHTML = `${data.main.temp}&deg`; 
+          temperature.innerHTML = `${Math.round(data.main.temp)}&deg`; 
           condition.innerHTML = `${data.weather[0].description}`;
           humidity.innerHTML = `${data.main.humidity}%`
-          wind.innerHTML = `${data.wind.speed}mph`
+          wind.innerHTML = `${Math.floor(data.wind.speed)}mph`
       })
       .catch((error) => {
           alert('Invalid city entry');

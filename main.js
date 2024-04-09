fc2150837653cf2c1e4e32499b290fe10b7482fc
kelseywarren <warrenkelsey@outlook.com>
2024-04-09 07:45:20 -0500

    forecast update 1.0

--- main.js
+++ main.js
@@ -2,8 +2,12 @@
 const apiKey = '4d76c17901d775c10628769986f03d11';
 const apiBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-//variables
+const forecastApiKey = '0bd62f2d32009d6c37c47b15082707e3'; 
+
+//default variables
 let city = 'austin';
+let lat = 30.27;
+let lon = -97.74; 
 
 //dom variables
 const searchInput = document.getElementById('searchInput');
@@ -20,6 +24,7 @@ searchButton.addEventListener('click', (e) => {
   if(city.length > 0){
     e.preventDefault();
     getWeatherData(city);
+    getCords(city);
   } else {
     e.preventDefault();
     alert('please enter city')
@@ -42,7 +47,7 @@ function getWeatherData(city) {
 
           humidity.innerHTML = `${data.main.humidity}%`
 
-          document.getElementById('wicon').src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+          document.getElementById('wicon').src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
           wind.innerHTML = `${Math.floor(data.wind.speed)}mph`
       })
@@ -51,8 +56,61 @@ function getWeatherData(city) {
           
       });
 };
-
-//default display
 getWeatherData(city);
 
+//function for getting coordinates
+function getCords(city) {
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${forecastApiKey}`;
 
+  fetch(geoUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    let lat = `${data[0].lat}`
+    let lon = `${data[0].lon}`
+    //console.log(lat)
+    //console.log(lon)
+    getForecast(lat,lon);
+  })
+  .catch((error) => alert('could not get cords'));
+ 
+};
+getCords(city);
+
+//function for getting forecast
+function getForecast(lat, lon) {
+  const forecastUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${forecastApiKey}&units=imperial`;
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      //loop to iterate the max temp for next 5 days
+      for(i = 0; i < 5; i++){
+        document.getElementById("day" + (i+1) + "Temp").innerHTML = `${data.daily[i].temp.max}`
+      };
+      //loop to iterate the weather icon for next 5 days
+      for(i = 0; i < 5; i++){
+        document.getElementById("day" + (i+1) + "Img").src = `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`;
+        };
+  
+    })
+    .catch((error) => alert('could not receive forecast')); 
+
+};
+
+//variables for day
+let d = new Date();
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+//function to check day
+function checkDay(day){
+  if(day + d.getDay() > 6){
+    return day + d.getDay() - 7;
+  } else {
+    return day + d.getDay();
+  }
+}
+
+//loop to iterate through the next 5 days
+for(i = 0; i < 5; i++){
+  document.getElementById("day" + (i+1)).innerHTML = days[checkDay(i)];
+}
--- test.js
+++ test.js
@@ -1,24 +1,38 @@
-/*const cars = ['WRX', 'MX-5', 'Cooper']
-console.log(cars)
-
-const myCars = cars.map((car)=> {
-   return car[0];
-} );
-console.log(myCars)*/
-
-const apiKey = '4d76c17901d775c10628769986f03d11';
-const apiBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const forecastApiKey = '0bd62f2d32009d6c37c47b15082707e3'; 
 
 let city = 'austin';
+//let lat = 30.27;
+//let lon = -97.74;
 
-function getWeatherData(city) {
-    const urlToFetch = `${apiBaseUrl}?q=${city},US&appid=${apiKey}&units=imperial`;//----- added ,US after ${city}. Fixed Manor temp being wrong -----// 
-    
-    fetch(urlToFetch)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
+
+function getCords(city) {
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},US&appid=${forecastApiKey}`;
+
+  fetch(geoUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    let lat = `${data[0].lat}`
+    let lon = `${data[0].lon}`
+
+    console.log(lat)
+    console.log(lon)
+    getForecast(lat,lon);
+  })
+  .catch((error) => alert('could not get cords'));
+ 
 };
+getCords(city);
 
-getWeatherData(city);
+
+function getForecast(lat, lon) {
+  const forecastUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${forecastApiKey}&units=imperial`
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+    console.log(data)
+    console.log(data.daily[0].temp.max)
+    })
+    .catch((error) => alert('could not receive forecast')); 
+};
+//getForecast(lat, lon);
